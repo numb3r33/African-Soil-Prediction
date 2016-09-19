6da90a1f67e41bc743ede1b0153b9e788b3d346e
Abhishek <abhicoders@gmail.com>
2016-09-19 15:54:59 +0530

    Modify class definition:

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,14 +2,28 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 class Data:
-    def __init__(self, train, test):
+    def __init__(self, train, test, remove_CO2_features=False):
         self.train = train
         self.test = test
+        self.remove_CO2_features = remove_CO2_features
     
     def concat_data(self):
         self.data = pd.concat((self.train, self.test), axis=0)
         return self.data
-    
+
+    def prepare(self):
+        features = self.non_infrared_features()
+        self.concat_data()
+        self.encode_categorical_features('Depth')
+        train_, test_ = self.get_train_test()
+        
+        if self.remove_CO2_features:
+            features = self.remove_CO2_band(features)
+            train_ = train_[features]
+            test_ = test_[features]
+
+        return train_[features], test_[features]
+
     def non_infrared_features(self):
         return self.train.columns[1:-5]
     
