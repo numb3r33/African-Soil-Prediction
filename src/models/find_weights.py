c096238b19f5bfb49abc1a90d8350deafd440751
Abhishek <abhicoders@gmail.com>
2016-09-23 00:40:02 +0530

    Change arguments.

--- src/models/find_weights.py
+++ src/models/find_weights.py
@@ -7,9 +7,9 @@ def weight_selected(data, labels):
     return weights
 
 
-def stack_predictions(*preds):
+def stack_predictions(preds):
 	# preds contain list of predictions for a given target variable
-	return np.vstack([*preds]).T
+	return np.vstack(preds).T
 	
 def calculate_weights(y_true, y_pred):
 	return weight_selected(y_pred, y_true)
@@ -26,7 +26,7 @@ def find(y_test, y_pred):
 	Returns: weights and the balanced prediction
 	"""
 
-	stacked_predictions = stack_predictions(*y_pred)
+	stacked_predictions = stack_predictions(y_pred)
 	weights = calculate_weights(y_test, stacked_predictions)
 
 	return weights, balance_predictions(y_test, stacked_predictions, weights)
