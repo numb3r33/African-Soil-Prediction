9527ee73c7b548db19fed97b9b0bda6b1ce7cb1a
Abhishek <abhicoders@gmail.com>
2016-09-23 00:39:47 +0530

    Add predictions to this module.

--- src/helper/utils.py
+++ src/helper/utils.py
@@ -37,3 +37,20 @@ def get_Ys(y_Ca, y_P, y_Sand, y_SOC, y_pH, itrain, itest):
 	
 	return ([y_train_Ca, y_train_P, y_train_Sand, y_train_SOC, y_train_pH],
 			[y_test_Ca, y_test_P, y_test_Sand, y_test_SOC, y_test_pH])
+
+def predict_targets(trained_models, Xs):
+    """
+    trained_models : List of the trained models
+    Xs             : Held out examples for each of the target variables
+    """
+    
+    n_target = len(Xs)
+    n_models = len(trained_models[0])
+    
+    predictions = np.empty((n_target, n_models), dtype=np.ndarray)
+    
+    for i in range(n_target):
+        for j in range(n_models):
+            predictions[i, j] = trained_models[i, j].predict(Xs[i])
+        
+    return predictions
