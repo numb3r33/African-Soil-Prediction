41a7c5340961d0aef01db7d6cbe346bc51848a74
Abhishek <abhicoders@gmail.com>
2016-09-23 13:21:23 +0530

    Refactor

--- src/helper/experiment.py
+++ src/helper/experiment.py
@@ -2,13 +2,6 @@ import pandas as pd
 import numpy as np
 import os, sys
 
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.decomposition import RandomizedPCA
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import f_classif, SelectKBest
-from sklearn.svm import SVR
-from sklearn.externals import joblib
-
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -19,56 +12,6 @@ np.random.seed(2)
 
 from models import cross_validation, eval_metric, models_definition, find_weights
 from helper import utils
-
-
-# load a dataset
-def load_dataset(train_filepath, test_filepath):
-	train_    = joblib.load(os.path.join(basepath, train_filepath))
-	test_     = joblib.load(os.path.join(basepath, test_filepath))
-	
-	return train_, test_
-
-# define target variables
-def define_target_variables(train):    
-	
-	y_Ca    = train.Ca
-	y_P     = train.P
-	y_Sand  = train.Sand
-	y_SOC   = train.SOC
-	y_pH    = train.pH
-	
-	return y_Ca, y_P, y_Sand, y_SOC, y_pH
-
-	
-# train models for all of the target varibles
-def train_models(models, Xs, ys):
-	"""
-	models : List of models that should be trained on a given (X, y)
-	Xs     : List of feature set for all of the target variables
-	ys     : List of the target variables
-	"""
-	
-	n_target = len(ys)
-	n_models = len(models)
-	
-	trained_models = np.empty((n_target, n_models), dtype=Pipeline)
-	
-	for i in range(n_target):
-		for j in range(n_models):
-			trained_models[i, j] = models[j].fit(Xs[i], ys[i])
-
-	return trained_models
-
-def save_models(trained_models, dataset_name):
-	labels = ['Ca', 'P', 'Sand', 'SOC', 'pH']
-	model_names  = ['rbf', 'linear', 'poly']
-	
-	for i in range(len(labels)):
-		for j in range(len(model_names)):
-			filepath = 'data/processed/%s/models/%s/%s/%s'%(dataset_name, labels[i], model_names[j], model_names[j])
-			joblib.dump(trained_models[i, j], os.path.join(basepath, filepath))
-
-	print('All models written to disk successfully')
 
 if __name__ == '__main__':
 	dataset_name = sys.argv[1]
@@ -83,10 +26,10 @@ if __name__ == '__main__':
 	train_filepath = 'data/processed/%s/train/train'%dataset_name
 	test_filepath  = 'data/processed/%s/test/test'%dataset_name
 
-	train_, test_  = load_dataset(train_filepath, test_filepath)	
+	train_, test_  = utils.load_dataset(train_filepath, test_filepath)	
 
 
-	y_Ca, y_P, y_Sand, y_SOC, y_pH = define_target_variables(train)
+	y_Ca, y_P, y_Sand, y_SOC, y_pH = utils.define_target_variables(train)
 
 	# lets get the train and test indices
 
@@ -107,8 +50,8 @@ if __name__ == '__main__':
 	models = models_definition.get_models_by_dataset(dataset_name)
 
 	print('Training Models\n')
-	trained_models = train_models(models, [X_train, X_train, X_train, X_train, X_train], \
+	trained_models = utils.train_models(models, [X_train, X_train, X_train, X_train, X_train], \
 									  [y_train_Ca, y_train_P, y_train_Sand, y_train_SOC, y_train_pH])
 
 	print('Save Models\n')
-	save_models(trained_models, dataset_name)
+	utils.save_models(trained_models, dataset_name)
