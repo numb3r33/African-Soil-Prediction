962d719201294721e304d436ab81aaa5d8a4a90f
Abhishek <abhicoders@gmail.com>
2016-09-24 00:28:05 +0530

    Change model definitions for some of the datasets.

--- src/models/models_definition.py
+++ src/models/models_definition.py
@@ -3,82 +3,101 @@ from sklearn.decomposition import RandomizedPCA, PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from sklearn.ensemble import BaggingRegressor
+from sklearn.linear_model import Ridge
 
 def get_models_by_dataset(dataset_name):
-    """
-    Since we will be training models depending on the dataset
-    we are associating models with the dataset. So we can pass in the dataset name
-    and based on that we will get a list of the models that should be trained on this dataset
-    based on our initial experiments
-    """
-    
-    models = {
-        'dataset_1': [
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='rbf'))
-            ]),
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='linear'))
-            ]),
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='poly'))
-            ])
-        ],
-        
-        'dataset_2': [
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='rbf'))
-            ]),
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='linear'))
-            ]),
-            Pipeline([
-                ('pca', PCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='poly'))
-            ])
-        ],
-        'dataset_3': [
-            Pipeline([
-                ('scaler', StandardScaler()),
-                ('model', (SVR(kernel='rbf')))
-            ]),
-            Pipeline([
-                ('scaler', StandardScaler()),
-                ('model', (SVR(kernel='linear')))
-            ]),
-            Pipeline([
-                ('scaler', StandardScaler()),
-                ('model', (SVR(kernel='poly')))
-            ])
-        ],
-        'dataset_4': [
-            Pipeline([
-                ('pca', RandomizedPCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='rbf'))
-            ]),
-            Pipeline([
-                ('pca', RandomizedPCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='linear'))
-            ]),
-            Pipeline([
-                ('pca', RandomizedPCA(n_components=125, whiten=True)),
-                ('scaler', StandardScaler()),
-                ('model', SVR(kernel='poly'))
-            ])
-        ]
-    }
+	"""
+	Since we will be training models depending on the dataset
+	we are associating models with the dataset. So we can pass in the dataset name
+	and based on that we will get a list of the models that should be trained on this dataset
+	based on our initial experiments
+	"""
+	
+	models = {
+		'dataset_1': [
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='rbf'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='linear'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='poly'))
+			]
+		],
+		
+		'dataset_2': [
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='rbf'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='linear'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='poly'))
+			]
+		],
+		'dataset_3': [
+			[
+				('scaler', StandardScaler()),
+				('model', (SVR(kernel='rbf')))
+			],
+			[
+				('scaler', StandardScaler()),
+				('model', (SVR(kernel='linear')))
+			],
+			[
+				('scaler', StandardScaler()),
+				('model', (SVR(kernel='poly')))
+			]
+		],
+		'dataset_4': [
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='rbf'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='linear'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=125, whiten=True)),
+				('model', SVR(kernel='poly'))
+			]
+		],
+		'dataset_5': [
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=10, whiten=True)),
+				('model', SVR(C=10.0, kernel='rbf'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=100, whiten=True)),
+				('model', SVR(C=10.0, kernel='linear'))
+			],
+			[
+				('scaler', StandardScaler()),
+				('pca', PCA(n_components=100, whiten=True)),
+				('model', SVR(C=10.0, kernel='poly'))
+			],
 
-    return models[dataset_name]
+		]
+	}
+
+	return models[dataset_name]
