ed3f436103cf17bd7439aa73f267774a218b08aa
Abhishek <abhicoders@gmail.com>
2016-09-26 23:55:42 +0530

    Rewrote cross validation scheme.

--- src/models/cross_validation.py
+++ src/models/cross_validation.py
@@ -1,42 +1,55 @@
 import os, sys
 
 from sklearn.cross_validation import KFold, train_test_split
+from sklearn.pipeline import Pipeline
 
 basepath = os.path.expanduser('~/Desktop/src/African_Soil_Property_Prediction/')
 sys.path.append(os.path.join(basepath, 'src'))
 
 from models import eval_metric
 
-def cv_scheme(pipelines, Xs, ys):
+def cv_scheme(model, Xs, ys):
+	"""
+	model : Model definition
+	Xs    : List of the dataframes for each of the target variables
+	ys    : List of the target variables
+
+	"""
+
+	# TODO: hard code this configuration as of now, will find a way to modify this.
 	cv = KFold(len(Xs[0]), n_folds=10, shuffle=True, random_state=10)
 	
+	
+	model = Pipeline(model)
+		
 	scores = 0
 	index = 0
 
 	for itrain, itest in cv:
-		index   = index + 1
+		y_true = []
+		y_pred = []
+		print('Fold: %d'%index)
+		index  += 1
 
-		y_preds = []
-		y_true  = []
-		
-		for i in range(len(pipelines)):
+		for i in range(len(ys)):
 			Xtr = Xs[i].iloc[itrain]
 			ytr = ys[i].iloc[itrain]
 
 			Xte = Xs[i].iloc[itest]
 			yte = ys[i].iloc[itest]
 
-			pipelines[i].fit(Xtr, ytr)
-			pred = pipelines[i].predict(Xte)
-			
-			y_true.append(yte)
-			y_preds.append(pred)
+			model.fit(Xtr, ytr)
 
-		score = eval_metric.mcrmse(y_true, y_preds)
-		
-		scores = scores + score
-	
+			y_true.append(yte)
+			y_pred.append(model.predict(Xte))
+
+		score = eval_metric.mcrmse(y_true, y_pred)
+		print('MCRMSE score: %f\n'%score)
+		scores += score
+
 	return scores / len(cv)
+
+
 
 
 def split_dataset(train_length, **params):
